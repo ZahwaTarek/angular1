62d3872c3cb51b02e40f4868083d0231eef12599
zahwa <zahwaelbakry@gmail.com>
2024-08-07 22:06:51 +0300

    first commit

---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+import { FooterComponent } from "../footer/footer.component";
+
+@Component({
+  selector: 'app-portfolio',
+  standalone: true,
+  imports: [NavbarComponent, FooterComponent],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.css'
+})
+export class PortfolioComponent {
+
+ 
+}
